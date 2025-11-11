ebc5246748c683aa74a761a73ddf945138b889e5
sumonkhan0077 <sumonkhanbd0077@gmail.com>
2025-11-11 10:02:37 +0600

    added email to data resive

--- index.js
+++ index.js
@@ -1,118 +1,137 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const cors = require('cors')
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
 // PCpxLC201swo4V5s
-const uri = "mongodb+srv://export_import:PCpxLC201swo4V5s@cluster0.abef6se.mongodb.net/?appName=Cluster0";
+const uri =
+  "mongodb+srv://export_import:PCpxLC201swo4V5s@cluster0.abef6se.mongodb.net/?appName=Cluster0";
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 // middleware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req , res) => {
-    res.send('export import server is running ')
-})
+app.get("/", (req, res) => {
+  res.send("export import server is running ");
+});
 
 async function run() {
-  try{
-    await  client.connect();
+  try {
+    await client.connect();
 
-    const db = client.db('export_import');
-    const productsCollection = db.collection('products');
-    const myCollection = db.collection('myProducts')
-    const usersCollection = db.collection('users')
+    const db = client.db("export_import");
+    const productsCollection = db.collection("products");
+    const myCollection = db.collection("myProducts");
+    const myImportCollection = db.collection("myImport");
 
-    // user data 
-    app.post('/users' , async (req , res ) => {
+    const usersCollection = db.collection("users");
+
+    // user data
+    app.post("/users", async (req, res) => {
       const newUser = req.body;
 
       const email = req.body.email;
-      const query = {email: email};
+      const query = { email: email };
       const existingUser = await usersCollection.findOne(query);
       if (existingUser) {
-         res.send('user already exits , do not need to add user')
-      }
-      else{
+        res.send("user already exits , do not need to add user");
+      } else {
         const result = await usersCollection.insertOne(newUser);
-        res.send(result)
-
+        res.send(result);
       }
-    })
+    });
 
-    // data pawa jabe 
-    app.get('/products', async (req , res ) => {
-        const cursor = productsCollection.find();
-        const result = await cursor.toArray();
-        res.send(result)
-    })
+    app.post("/myImport", async (req, res) => {
+      const newProducts = req.body;
+      const result = await myImportCollection.insertOne(newProducts);
+      res.send(result);
+    });
+
+    app.get("/myImport", async (req, res) => {
+      const email = req.query.email; 
+
+      let query = {};
+      if (email) {
+        query = { buyer_email: email };
+      }
+
+      const cursor = myImportCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // data pawa jabe
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // latest products
-    app.get('/latest-products' , async (req, res) =>{
-      const cursor = productsCollection.find().sort({time: -1}).limit(6);
+    app.get("/latest-products", async (req, res) => {
+      const cursor = productsCollection.find().sort({ time: -1 }).limit(6);
       const result = await cursor.toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     // data pabo id te
-    app.get('/products/:id' , async (req , res ) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id)};
-        const result = await productsCollection.findOne(query);
-        res.send(result)
-    })
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
 
     // data pathaschi
-    app.post('/products', async (req , res) => {
-        const newProducts = req.body;
-        const result = await productsCollection.insertOne(newProducts);
-        res.send(result);
-    })
+    app.post("/products", async (req, res) => {
+      const newProducts = req.body;
+      const result = await productsCollection.insertOne(newProducts);
+      res.send(result);
+    });
 
     // update data
-     app.patch('/products/:id' , async(req , res ) => {
-        const id = req.params.id;
-        const updatedProduct = req.body;
-        const query = {_id : new ObjectId(id)}; 
-        const update = {
-            $set: {
-                name: updatedProduct.name,
-                price: updatedProduct.price
-            }
-        }
-        const result = await productsCollection.updateOne(query , update);
-        res.send(result)
-     })
+    app.patch("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedProduct = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: {
+          name: updatedProduct.name,
+          price: updatedProduct.price,
+        },
+      };
+      const result = await productsCollection.updateOne(query, update);
+      res.send(result);
+    });
 
     // delete data
-    app.delete('/products/:id' , async (req , res) => {
-        const id = req.params.id;
-        const query  = {_id: new ObjectId(id)};
-        const result = await productsCollection.deleteOne(query);
-        res.send(result)
-    })
-    
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  }
-  finally{
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
     //  await client.close();
   }
-    
 }
 
-run().catch(console.dir)
+run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
