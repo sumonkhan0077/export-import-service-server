9480751b5507e2382bc5151528837144b75ac111
sumonkhan0077 <sumonkhanbd0077@gmail.com>
2025-11-12 23:36:31 +0600

    added env for user and password safe

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// PCpxLC201swo4V5s
-const uri =
-  "mongodb+srv://export_import:PCpxLC201swo4V5s@cluster0.abef6se.mongodb.net/?appName=Cluster0";
+
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.abef6se.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -51,15 +51,15 @@ async function run() {
     });
 
     app.post("/myImport", async (req, res) => {
-      const newImport  = req.body;
+      const newImport = req.body;
       const quantity = parseInt(newImport.quantity);
       const result = await myImportCollection.insertOne(newImport);
-      const filter = { _id: new ObjectId(newImport.product_id)}
+      const filter = { _id: new ObjectId(newImport.product_id) };
       const update = {
-        $inc :{ available_quantity: -quantity}
-      }
-      const countQuantity = await productsCollection.updateOne(filter , update)
-      res.send(result , countQuantity);
+        $inc: { available_quantity: -quantity },
+      };
+      const countQuantity = await productsCollection.updateOne(filter, update);
+      res.send(result, countQuantity);
     });
 
     // email diya pawa
@@ -100,7 +100,7 @@ async function run() {
         }
 
         const result = await productsCollection.find(query).toArray();
-        console.log("Matched products:", result.length);
+        // console.log("Matched products:", result.length);
         res.send(result);
       } catch (error) {
         // console.error("Error fetching products:", error);
@@ -153,14 +153,14 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const update = {
         $set: {
-        product_image: updatedProduct.product_image,
-        product_name: updatedProduct.product_name,
-        price: updatedProduct.price,
-        origin_country: updatedProduct.origin_country,
-        rating: updatedProduct.rating,
-        rating_number: updatedProduct.rating_number,
-        available_quantity: updatedProduct.available_quantity,
-        description: updatedProduct.description,
+          product_image: updatedProduct.product_image,
+          product_name: updatedProduct.product_name,
+          price: updatedProduct.price,
+          origin_country: updatedProduct.origin_country,
+          rating: updatedProduct.rating,
+          rating_number: updatedProduct.rating_number,
+          available_quantity: updatedProduct.available_quantity,
+          description: updatedProduct.description,
         },
       };
       const result = await productsCollection.updateOne(query, update);
